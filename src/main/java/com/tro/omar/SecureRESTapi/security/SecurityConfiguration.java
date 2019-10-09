f31cb58ddc6f87b24071e2fd4e21b7c836102ad2
1426391 <ahmed.al-salih@delhaize.com>
2019-10-09 14:00:32 -0400

    PasswordEncoder beeing use

--- src/main/java/com/tro/omar/SecureRESTapi/security/SecurityConfiguration.java
+++ src/main/java/com/tro/omar/SecureRESTapi/security/SecurityConfiguration.java
@@ -1,12 +1,13 @@
 package com.tro.omar.SecureRESTapi.security;
 
-import java.net.PasswordAuthentication;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -17,9 +18,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
 				.inMemoryAuthentication() 
-				.withUser( "Admin").password("Admin123").roles("ADMIN")
+				.withUser( "Admin").password(passwordEncoder().encode("Admin123")).roles("ADMIN")
 				.and()
-				.withUser("user").password("password").roles("USER");
+				.withUser("user").password(passwordEncoder().encode("password")).roles("USER");
 	}//AuthenticationManagerBuilder
 	
 	@Override
@@ -30,6 +31,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.and()
 				.httpBasic();
 	}//HttpSecurity
+	
+	
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+		
+	}
 
 	
 }//SecurityConfiguration
